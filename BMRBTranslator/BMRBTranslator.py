9bc8e0e4c3df62facd5081fb9eba478491a21269
Kumaran <kumar.physics@gmail.com>
2017-01-18 13:22:31 -0600

    Initial commit

---
+++ BMRBTranslator/BMRBTranslator.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 18, 2017
+
+@author: root
+'''
+
+class BMRBTranslator(object):
+    '''
+    Main class for BMRBTranslator
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    
