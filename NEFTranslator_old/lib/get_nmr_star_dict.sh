ce5b8a8d2c69cf018686c7cf89aec5f1078ea4b4
Kumaran Baskaran <kumar.physics@gmail.com>
2021-03-22 23:00:35 -0400

    wwPDB NEFTranslator require other modules the old one works just fine

--- NEFTranslator_old/lib/get_nmr_star_dict.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget http://svn.bmrb.wisc.edu/svn/nmr-star-dictionary/bmrb_only_files/adit_input/NMR-STAR.dic
-wget http://svn.bmrb.wisc.edu/svn/nmr-star-dictionary/bmrb_only_files/adit_input/nmrstar3_fake.txt
